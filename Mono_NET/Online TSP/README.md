b8ac916bffc026a1e2300751c7eeec5302f4fe0c
xivk <ben@osmsharp.com>
2015-04-20 09:06:42 +0200

    Added TSPClient Sample.

---
+++ Mono_NET/Online TSP/README.md
@@ -0,0 +1,11 @@
+# Online TSP
+
+Demonstrates how to use OsmSharp and the [OsmSharp routing service](https://github.com/OsmSharp/OsmSharp.Service.Routing) to calculate a TSP-solution. The routing service does all the hard work, this code is a client that demonstrates how to sort a set of locations in a given CSV-file and output the same CSV-file but sorted along with the route.
+
+INPUT: 
+* CSV-file with lat-lon and other info.
+OUTPUT: 
+* CSV-file with lat-lon and other info BUT sorted along shortest path.
+* GPX-file with the route along all points in the CSV-file.
+ 
+To run this sample you need to setup the [OsmSharp routing service](https://github.com/OsmSharp/OsmSharp.Service.Routing) with the antwerpen.osm.pbf in this folder and use the configuration file antwerpen.config.
---
+++ Mono_NET/Online TSP/TSPClient/TSPClient/Program.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using OsmSharp.Routing;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace TSPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vehicle = "bicycle"; // the vehicle type to calculate for.
+            var instance = "antwerp"; // the instance name on the routing service.
+
+            // Read the CSV-data.
+            var lines = OsmSharp.IO.DelimitedFiles.DelimitedFileHandler.ReadDelimitedFile(null,
+                new FileInfo("bicylestations.csv").OpenRead(), OsmSharp.IO.DelimitedFiles.DelimiterType.DotCommaSeperated, true);
+
+            // Build the request.
+            var query = "/routing?";
+            query = query + "vehicle=" + vehicle;
+            for(int idx = 0; idx < lines.Length; idx++)
+            {
+                var line = lines[idx];
+                // parse first.
+                var lat = double.Parse(line[0], System.Globalization.CultureInfo.InvariantCulture);
+                var lon = double.Parse(line[1], System.Globalization.CultureInfo.InvariantCulture);
+                query = query + "&loc=" + lat.ToString(System.Globalization.CultureInfo.InvariantCulture) + "," + lon.ToString(System.Globalization.CultureInfo.InvariantCulture);
+            }
+            query = query + "&sort=true&format=osmsharp"; // make sure the service does the TSP-calculation.
+
+            // Read the response.
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(
+                "http://localhost:1234/" + instance + query);
+            httpWebRequest.Accept = "application/json";
+            httpWebRequest.Method = "GET";
+            var response = httpWebRequest.GetResponse(); // get the data.
+            Route route = null;
+            using (var responseStream = new StreamReader(response.GetResponseStream()))
+            {
+                var responseString = responseStream.ReadToEnd();
+                route = JsonConvert.DeserializeObject<Route>(responseString);
+            }
+
+
+            // Write output CSV-data.
+            var points = new List<int>();
+            foreach(var segment in route.Segments)
+            {
+                if (segment.Points == null) { continue; }
+                foreach(var point in segment.Points)
+                {
+                    if (point.Tags == null) { continue; }
+                    foreach(var tag in point.Tags)
+                    {
+                        if(tag.Key == "point_id")
+                        {
+                            points.Add(int.Parse(tag.Value));
+                            break;
+                        }
+                    }
+                }
+            }
+            var sortedLines = new string[points.Count][];
+            for(int idx = 0; idx < points.Count; idx++)
+            {
+                sortedLines[idx] = lines[points[idx]];
+            }
+            using(var csvStream = new StreamWriter((new FileInfo("output.csv").OpenWrite())))
+            {
+                OsmSharp.IO.DelimitedFiles.DelimitedFileHandler.WriteDelimitedFile(null, sortedLines, 
+                    csvStream, OsmSharp.IO.DelimitedFiles.DelimiterType.DotCommaSeperated);
+            }
+
+            // Write GPX-data.
+            using(var gpxStream = (new FileInfo("output.gpx").OpenWrite()))
+            {
+                route.SaveAsGpx(gpxStream);
+            }
+        }
+    }
+}
